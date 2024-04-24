65e355a90cd16dc329f2d7bc9db987ac735bc2fd
tone4hook <antone4hook@gmail.com>
2024-04-24 17:52:24 +0900

    Chore: env flag for host

---
+++ README.md
@@ -0,0 +1,35 @@
+# Currency Vista
+
+Currency Vista is a Single Page Application (SPA) that showcases a Micro Frontends architecture using Webpack Module Federation. It features a host SPA consuming two independently deployable apps, a Currency Converter and a Dictionary. All applications are hosted on GitHub Pages.
+
+## Scripts
+
+- `build`: Builds the application for production.
+- `build:dev`: Builds the application for development.
+- `start`: Starts the development server and opens the application in a web browser.
+- `start:live`: Starts the development server with live reloading and opens the application in a web browser.
+- `lint`: Lints the codebase.
+- `lint:fix`: Lints the codebase and automatically fixes any fixable issues.
+- `format`: Formats the codebase using Prettier.
+
+## Author
+
+- Anthony Hook
+
+## License
+
+This project is licensed under the MIT License.
+
+## Dependencies
+
+This project uses several dependencies, including:
+
+- Vue.js for building user interfaces.
+- Vue Router for routing.
+- Pinia for state management.
+- Bootstrap for styling.
+- Babel and TypeScript for transpiling and type checking.
+- ESLint and Prettier for linting and formatting.
+- Webpack for bundling and module federation.
+
+For a full list of dependencies, refer to the `package.json` file.
--- webpack.config.js
+++ webpack.config.js
@@ -5,75 +5,82 @@ const Dotenv = require('dotenv-webpack');
 const path = require('path');
 const packageJson = require('./package.json');
 
-module.exports = () => ({
-  output: {
-    publicPath: 'https://tone4hook.github.io/currency-vista/',
-  },
+module.exports = () => {
+  const isProduction = process.env.NODE_ENV === 'production';
+  const publicPath = isProduction
+    ? 'https://tone4hook.github.io/currency-vista/'
+    : 'http://localhost:8080/';
 
-  resolve: {
-    extensions: ['.tsx', '.ts', '.vue', '.jsx', '.js', '.json'],
-    alias: {
-      '@': path.resolve(__dirname, 'src/'),
+  return {
+    output: {
+      publicPath,
     },
-  },
 
-  devServer: {
-    port: 8080,
-    historyApiFallback: true,
-  },
+    resolve: {
+      extensions: ['.tsx', '.ts', '.vue', '.jsx', '.js', '.json'],
+      alias: {
+        '@': path.resolve(__dirname, 'src/'),
+      },
+    },
 
-  module: {
-    rules: [
-      {
-        test: /\.vue$/,
-        loader: 'vue-loader',
-      },
-      {
-        test: /\.tsx?$/,
-        use: [
-          'babel-loader',
-          {
-            loader: 'ts-loader',
-            options: {
-              transpileOnly: true,
-              appendTsSuffixTo: ['\\.vue$'],
-              happyPackMode: true,
-            },
-          },
-        ],
-      },
-      {
-        test: /\.(css|s[ac]ss)$/i,
-        use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
-      },
-      {
-        test: /\.(png|jpe?g|gif|svg|ico)$/i,
-        loader: 'file-loader',
-        options: {
-          name: '[path][name].[ext]',
+    devServer: {
+      port: 8080,
+      historyApiFallback: true,
+    },
+
+    module: {
+      rules: [
+        {
+          test: /\.vue$/,
+          loader: 'vue-loader',
         },
-      },
-    ],
-  },
+        {
+          test: /\.tsx?$/,
+          use: [
+            'babel-loader',
+            {
+              loader: 'ts-loader',
+              options: {
+                transpileOnly: true,
+                appendTsSuffixTo: ['\\.vue$'],
+                happyPackMode: true,
+              },
+            },
+          ],
+        },
+        {
+          test: /\.(css|s[ac]ss)$/i,
+          use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
+        },
+        {
+          test: /\.(png|jpe?g|gif|svg|ico)$/i,
+          loader: 'file-loader',
+          options: {
+            name: '[path][name].[ext]',
+          },
+        },
+      ],
+    },
 
-  plugins: [
-    new VueLoaderPlugin(),
-    new ModuleFederationPlugin({
-      name: 'currency_vista',
-      filename: 'remoteEntry.js',
-      remotes: {
-        currency_converter:
-          'currency_converter@https://tone4hook.github.io/currency-converter/remoteEntry.js',
-        vista_dictionary:
-          'vista_dictionary@https://tone4hook.github.io/vista-dictionary/remoteEntry.js',
-      },
-      exposes: {},
-      shared: packageJson.dependencies,
-    }),
-    new HtmlWebPackPlugin({
-      template: './src/index.html',
-      favicon: './src/assets/favicon.ico',
-    }),
-    new Dotenv(),
-  ],
-});
+    plugins: [
+      new VueLoaderPlugin(),
+      new ModuleFederationPlugin({
+        name: 'currency_vista',
+        filename: 'remoteEntry.js',
+        remotes: {
+          currency_converter:
+            'currency_converter@https://tone4hook.github.io/currency-converter/remoteEntry.js',
+          vista_dictionary:
+            'vista_dictionary@https://tone4hook.github.io/vista-dictionary/remoteEntry.js',
+        },
+        exposes: {},
+        shared: packageJson.dependencies,
+      }),
+      new HtmlWebPackPlugin({
+        template: './src/index.html',
+        favicon: './src/assets/favicon.ico',
+      }),
+      new Dotenv(),
+    ],
+  };
+};
